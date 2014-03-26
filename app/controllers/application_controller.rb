fb706651b1eed507d516dca6ab38c2430553903d
jun <gjtiger@gmail.com>
2014-03-26 08:52:26 +0800

    save work

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,10 +2,26 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  
+
   before_action :set_locale
+
+  before_filter :set_navigation_tabs
+
+  private
   
   def set_locale
     I18n.locale = params[:locale] || I18n.default_locale
+  end
+
+  def set_navigation_tabs
+    @tabs =
+    if current_user && manager?
+      [
+        { :name => "Home", :icon => "icon-home", :path => home_index_path },
+        { :name => "Portfolio", :icon => "icon-camera", :path => portfolio_home_index_path },
+        { :name => "Contact", :icon => "icon-envelope-alt", :path => contact_home_index_path }
+      ]
+    elsif current_user && client?
+    end
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,19 @@
 module ApplicationHelper
+  
+  # http://stackoverflow.com/questions/3705898/best-way-to-add-current-class-to-nav-in-rails-3
+  def nav_tabs(tabs=[])
+    html = []
+    tabs.each do |tab|
+      html << (content_tag :li, :class => ("active" if request.fullpath.split(/[\??]/)[0] == tab[:path]) do
+        link_to tab[:path] do
+          content_tag(:i, '', :class => tab[:icon]) +
+          tag(:br) +
+          "#{tab[:name]}"
+        end
+      end)
+    end
+
+    html.join.html_safe
+  end
+
 end
